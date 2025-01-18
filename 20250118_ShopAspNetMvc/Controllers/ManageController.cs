0c7c4f77a53bcfffbb865da3dc11a80bb4561bd0
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-01-18 20:39:08 +0300

    Add ManageController for product management
    Introduce ManageController class in ManageController.cs to manage products within the _20250118_ShopAspNetMvc.Controllers namespace. Utilize dependency injection to obtain an instance of IProductRepository for product data interaction. Implement Index action method to retrieve and display all products. Add Delete action method to remove products by ID and redirect to Index. Comment out Create, Edit, and DeleteConfirmed action methods. Include necessary using directives for Microsoft.AspNetCore.Mvc, _20250118_ShopAspNetMvc.Data, and _20250118_ShopAspNetMvc.Models.

---
+++ 20250118_ShopAspNetMvc/Controllers/ManageController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using _20250118_ShopAspNetMvc.Data;
+using _20250118_ShopAspNetMvc.Models;
+
+namespace _20250118_ShopAspNetMvc.Controllers
+{
+    public class ManageController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ManageController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productRepository.GetAllProducts().ToList();
+            return View(products);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = _productRepository.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _productRepository.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
+
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public IActionResult Create(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _productRepository.AddProduct(product);
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(product);
+        //}
+
+        //public IActionResult Edit(int id)
+        //{
+        //    var product = _productRepository.GetProductById(id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(product);
+        //}
+
+        //[HttpPost]
+        //public IActionResult Edit(Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _productRepository.UpdateProduct(product);
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(product);
+        //}
+
+
+        //[HttpPost, ActionName("Delete")]
+        //public IActionResult DeleteConfirmed(int id)
+        //{
+        //    _productRepository.DeleteProduct(id);
+        //    return RedirectToAction("Index");
+        //}
+    }
+}
