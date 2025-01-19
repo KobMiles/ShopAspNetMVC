6b0f65abb9e81a41549860ded744fc34acb3faf0
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-01-19 14:02:57 +0300

    Add ProductCategoryViewModel class to ViewModels
    Added ProductCategoryViewModel.cs with ProductCategoryViewModel class in _20250118_ShopAspNetMvc.ViewModels namespace. The class includes Product and Categories properties, and a constructor initializing Categories. Included using directive for _20250118_ShopAspNetMvc.Models.

---
+++ 20250118_ShopAspNetMvc/ViewModels/ProductCategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿using _20250118_ShopAspNetMvc.Models;
+
+namespace _20250118_ShopAspNetMvc.ViewModels
+{
+    public class ProductCategoryViewModel
+    {
+        public Product Product { get; set; }
+        public List<Category> Categories { get; set; }
+
+        public ProductCategoryViewModel(List<Category> categories)
+        {
+            Categories = categories;
+        }
+    }
+}
