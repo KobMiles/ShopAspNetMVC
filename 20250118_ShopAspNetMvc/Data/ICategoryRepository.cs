00e7f088177e6a01f8e8d6f6369c4a84de69c960
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-01-19 14:02:08 +0300

    Add ICategoryRepository interface for category retrieval
    A new interface `ICategoryRepository` has been added to the
    `_20250118_ShopAspNetMvc.Data` namespace. This interface includes
    a method `GetAllCategories()` which returns an `IEnumerable<Category>`.
    The `Category` model is used from the `_20250118_ShopAspNetMvc.Models`
    namespace to represent individual category items.

---
+++ 20250118_ShopAspNetMvc/Data/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using _20250118_ShopAspNetMvc.Models;
+
+namespace _20250118_ShopAspNetMvc.Data
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> GetAllCategories();
+    }
+}
