29ebbef7c5b9c4dd39c6ef5a8fb56f164bec5435
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-01-19 14:01:59 +0300

    Add DbSet<Category> to ApplicationDbContext
    Added a new DbSet<Category> property to the ApplicationDbContext class, allowing the context to manage Category entities in addition to the existing Product entities.

--- 20250118_ShopAspNetMvc/Data/ApplicationDbContext.cs
+++ 20250118_ShopAspNetMvc/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace _20250118_ShopAspNetMvc.Data
         }
         
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
